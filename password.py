14033e468b36bfe9603e318b816caaf45d2ead0f
Dustin <wardddustin@gmail.com>
2019-12-08 16:40:57 -0700

    Added theme

--- password.py
+++
@@ -1,9 +0,0 @@
-def salt_password(password):
-    i = 0
-    pass_arr = []
-    for char in password:
-        i += 1
-        pass_arr.append(char)
-        if i % 2 == 0:
-            pass_arr.append('$$%$')
-    return ''.join(pass_arr)
